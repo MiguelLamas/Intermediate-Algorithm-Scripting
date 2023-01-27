89f8daf4a0acf06c4aea79ae3ab46d7a21609e4e
Miguel Lamas <lamasmigs@yahoo.co.uk>
2023-01-27 20:04:53 +0000

    Sum All Numbers in a Range

--- sumNumsInRange.js
+++ sumNumsInRange.js
@@ -8,3 +8,14 @@ The lowest number will not always come first.
 For example, sumAll([4,1]) should return 10 because sum of all the numbers between 1 and 4 (both inclusive) is 10.
 */
 
+function sumAll(arr) {
+    let maxNum = Math.max(...arr); // find maxNum using ...spread operator
+    let minNum = Math.min(...arr); // find minNum
+    let sum = 0; // create a variable to store sum as we loop through array
+    for (let i = minNum; i <= maxNum; i++) {  // initialisation at minNum in arr, keep incrementing and adding to sum each iteration.
+      sum += i
+    }
+    return sum;
+  }
+  
+ sumAll([1, 4]);
