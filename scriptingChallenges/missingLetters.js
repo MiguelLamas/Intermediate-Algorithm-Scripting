449cdbd32cd1acc44eb66714136f4fda45177bd6
Miguel Lamas <lamasmigs@yahoo.co.uk>
2023-01-30 23:58:04 +0000

    Challenge Completed - Missing Letters

---
+++ scriptingChallenges/missingLetters.js
@@ -0,0 +1,16 @@
+/*
+Missing letters
+Find the missing letter in the passed letter range and return it.
+
+If all letters are present in the range, return undefined.
+*/
+
+function fearNotLetter(str) {
+    for (let i = 0; i < str.length; i++) {
+      if (str.charCodeAt(i) !== str.charCodeAt(0)+i) {
+         return String.fromCharCode(str.charCodeAt(0)+i)
+      }
+    }
+  }
+  
+  console.log(fearNotLetter("abce"));
