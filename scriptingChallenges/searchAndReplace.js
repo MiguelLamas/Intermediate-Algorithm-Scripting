0f706e249b67d1da3a34e3702c96d39fd59cd320
Miguel Lamas <lamasmigs@yahoo.co.uk>
2023-01-30 23:42:57 +0000

    Completed challenge - Search & Replace

--- scriptingChallenges/searchAndReplace.js
+++ scriptingChallenges/searchAndReplace.js
@@ -9,8 +9,8 @@ Note: Preserve the case of the first character in the original word when you are
 For example if you mean to replace the word Book with the word dog, it should be replaced as Dog */
 
 // 1. replace word in string 'before' with the word in 'after'
-// 2. If the word in 'after' starts with UpperCase keep first character as UpperCase
-// 3. Else convert first character of word into a lowerCase.
+// 2. If the word 'before starts with an uppercase, then convert first letter in 'after' to uppercase.
+// 3. Else make sure first letter of 'after' is lowercase too.
 
 function myReplace(str, before, after) {
     if(before.match(/^[A-Z]/)){
