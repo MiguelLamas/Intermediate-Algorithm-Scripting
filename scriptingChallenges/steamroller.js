67078b65b5f248d608a54865027f1df3984aa4eb
Miguel Lamas <lamasmigs@yahoo.co.uk>
2023-02-03 23:18:40 +0000

    Challenge completed - Steamroller

---
+++ scriptingChallenges/steamroller.js
@@ -0,0 +1,12 @@
+/*
+Steamroller -
+
+Flatten a nested array. You must account for varying levels of nesting.
+*/
+
+function steamrollArray(arr) {
+    let answer = [].concat(...arr)
+    return answer.some(Array.isArray) ? steamrollArray(answer) : answer;
+  }
+  
+  steamrollArray([1, [2], [3, [[4]]]]); // [ 1, 2, 3, 4 ]
